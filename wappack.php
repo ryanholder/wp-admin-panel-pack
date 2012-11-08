1b4eec272c3cb49c2bc2337dcc5d4ce29bf5cd64
Ryan Holder <ryan@ryanholder.com>
2012-11-08 18:26:58 +0200

    WAPPack changes;
    - Added a function to add page hooks to Help Panel
    - Unset dashboard widgets

--- wappack.php
+++ wappack.php
@@ -51,12 +51,11 @@ class WordPressAdminPanelPack {
 		// Register site styles and scripts
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
-		
+
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 		
 	    /*
-	     * TODO:
 	     * Define the custom functionality for your plugin. The first parameter of the
 	     * add_action/add_filter calls are the hooks into which your code should fire.
 	     *
@@ -67,8 +66,14 @@ class WordPressAdminPanelPack {
 	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
 	     */
 
+        /*
+         * This adds to the admin screen help panel listing all page hooks
+         */
+        add_action( 'contextual_help', array( $this, 'wptuts_screen_help' ), 10, 3 );
 
 	    add_action( 'admin_head', array( $this, 'wappack_admin_head' ) );
+
+        add_action( 'admin_menu', array( $this, 'wappack_disable_default_dashboard_widgets' ) );
 
 	} // end constructor
 	
@@ -78,8 +83,8 @@ class WordPressAdminPanelPack {
 	 * @params	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
 	 */
 	public function activate( $network_wide ) {
-		// TODO define activation functionality here
-	} // end activate
+
+    } // end activate
 	
 	/**
 	 * Fired when the plugin is deactivated.
@@ -138,9 +143,65 @@ class WordPressAdminPanelPack {
 	 *		  Action Reference:  http://codex.wordpress.org/Plugin_API/Action_Reference
 	 *
 	 */
+
+
+    /*
+     * This function is in place during development and must be removed
+     * TODO Remove Screen Help Function
+     */
+    function wptuts_screen_help( $contextual_help, $screen_id, $screen ) {
+        // The add_help_tab function for screen was introduced in WordPress 3.3.
+        if ( ! method_exists( $screen, 'add_help_tab' ) )
+            return $contextual_help;
+        global $hook_suffix;
+        // List screen properties
+        $variables = '<ul style="width:50%;float:left;"> <strong>Screen variables </strong>'
+            . sprintf( '<li> Screen id : %s</li>', $screen_id )
+            . sprintf( '<li> Screen base : %s</li>', $screen->base )
+            . sprintf( '<li>Parent base : %s</li>', $screen->parent_base )
+            . sprintf( '<li> Parent file : %s</li>', $screen->parent_file )
+            . sprintf( '<li> Hook suffix : %s</li>', $hook_suffix )
+            . '</ul>';
+        // Append global $hook_suffix to the hook stems
+        $hooks = array(
+            "load-$hook_suffix",
+            "admin_print_styles-$hook_suffix",
+            "admin_print_scripts-$hook_suffix",
+            "admin_head-$hook_suffix",
+            "admin_footer-$hook_suffix"
+        );
+        // If add_meta_boxes or add_meta_boxes_{screen_id} is used, list these too
+        if ( did_action( 'add_meta_boxes_' . $screen_id ) )
+            $hooks[] = 'add_meta_boxes_' . $screen_id;
+        if ( did_action( 'add_meta_boxes' ) )
+            $hooks[] = 'add_meta_boxes';
+        // Get List HTML for the hooks
+        $hooks = '<ul style="width:50%;float:left;"> <strong>Hooks </strong> <li>' . implode( '</li><li>', $hooks ) . '</li></ul>';
+        // Combine $variables list with $hooks list.
+        $help_content = $variables . $hooks;
+        // Add help panel
+        $screen->add_help_tab( array(
+            'id'      => 'wptuts-screen-help',
+            'title'   => 'Screen Information',
+            'content' => $help_content,
+        ));
+        return $contextual_help;
+    }
+
 	function wappack_admin_head() {
     	// TODO define your action method here
 	} // end action_method_name
+
+    function wappack_disable_default_dashboard_widgets() {
+        remove_meta_box( 'dashboard_right_now', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_plugins', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_primary', 'dashboard', 'core' );
+        remove_meta_box( 'dashboard_secondary', 'dashboard', 'core' );
+    }
 
 } // end class
 
