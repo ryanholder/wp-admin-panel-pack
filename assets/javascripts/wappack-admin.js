98515019b12da4cfa40381dd65cf1655e4e5050d
Ryan Holder <ryan@ryanholder.com>
2012-11-28 22:41:07 +0200

    change to add badge class to span for comment and plugin counts

--- assets/javascripts/wappack-admin.js
+++ assets/javascripts/wappack-admin.js
@@ -8,11 +8,10 @@
         $('#adminmenu').children('.menu-top').tooltip({
             title:function () {
 
-                $(this).find('span.pending-count').removeClass('pending-count').addClass('badge');
-                var title = $(this).children('.menu-top').html(); //TODO add badge class to the inner span
+                $(this).find('span > span').removeClass().addClass('badge'); //TODO concerned the span > span might not always be true, need better solution
+                var title = $(this).children('.menu-top').html();
                 return title;
             },
-            trigger: 'click',
             placement: 'right',
             html: 'true'
 
