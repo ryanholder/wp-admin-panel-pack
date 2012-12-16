1b2883496f4b0816fa6bd111be23552852188c05
David Laing <david@davidlaing.com>
2012-12-16 23:27:54 +0000

    Fix error: Only a static class method or function can be used in an uninstall hook. Please see Debugging in WordPress for more information. (This message was added in version 3.1.)

--- wappack.php
+++ wappack.php
@@ -61,7 +61,7 @@ class WordPressAdminPanelPack {
 
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
-		register_uninstall_hook( __FILE__, array( $this, 'uninstall' ) );
+		register_uninstall_hook( __FILE__, 'WordPressAdminPanelPack::uninstall' );
 
 		/*
 		 * Define the custom functionality for your plugin. The first parameter of the
@@ -109,7 +109,7 @@ class WordPressAdminPanelPack {
 	 *
 	 * @params    $network_wide    True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
 	 */
-	public function uninstall( $network_wide ) {
+	public static function uninstall( $network_wide ) {
 		// TODO define uninstall functionality here
 	} // end uninstall
 
